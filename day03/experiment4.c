82e2d9e56e89010a5b66eab5a53779e0b4ecbd9c
Rahul <rahul.sisodia2025@glbajajgroup.org>
2026-06-11 21:48:31 +0530

    third day assignment completed

---
+++ day03/experiment4.c
@@ -0,0 +1,32 @@
+// Write a program to Find LCM of two numbers.
+
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int lcm(int a, int b) {
+    return (a / gcd(a, b)) * b;
+}
+
+int main() {
+    int num1, num2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    if (num1 <= 0 || num2 <= 0) {
+        printf("Please enter positive integers.\n");
+        return 1;
+    }
+
+    printf("LCM of %d and %d is %d\n", num1, num2, lcm(num1, num2));
+
+    return 0;
+}
