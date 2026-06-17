fe747318c76238bb5674b22f414137f4cb7c38cc
Rahul <rahul.sisodia2025@glbajajgroup.org>
2026-06-17 20:31:14 +0530

    day 8 assignment completed

---
+++ day08/experiment1.c
@@ -0,0 +1,21 @@
+// Write a program to Print half pyramid pattern.
+
+
+#include <stdio.h>
+
+int main() {
+    int rows;
+    
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    
+    // Print half pyramid pattern using stars
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
